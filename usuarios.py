d4a1f1c908a4156470600395c86d2e63bc89cf6d
D45-csm <acos79.0.98dan1lec@gmail.com>
2026-06-17 10:09:04 -0500

    usuarios

---
+++ usuarios.py
@@ -0,0 +1,11 @@
+class Usuario:
+    id_usuario = 1
+    documento = "111010101"
+    nombre = "Daniel"
+    apellido = "Acosta"
+    correo= "Dnaiiii@.com"
+    telefono= "344432442"
+    direccion = "cll 4 3-2-1"
+
+usuario1= Usuario()
+
